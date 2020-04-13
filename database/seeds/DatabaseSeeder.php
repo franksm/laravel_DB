84dd0f7b22ead5d5a07e82a9a1092b7366c3ec1a
franksm <franksmhuang@gmail.com>
2020-04-13 15:20:21 +0800

    Modify S Database[Foreign key constraint]

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        
         $this->call(PeopleSeeder::class);
         $this->call(InterestSeeder::class);
         $this->call(PeopleInterestSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
